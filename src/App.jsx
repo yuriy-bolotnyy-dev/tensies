3ed3e63c6b269ff2338a1ac8cccd0b18ed875c4d
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-10-28 17:52:42 -0700

    Add isHeld prop

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,6 @@ import '../style.css'
 
 import { nanoid } from 'nanoid'
 
-
-/**
- * Challenge:
- * 
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components 
- *   to look like they do in the slide
- *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *      - Use flexbox on main to center the dice container
- *        in the center of the page
- */
-
-  
-
   const App = () => {
     const randonNum = () => (Math.floor(Math.random() * 7))
 
@@ -30,14 +11,14 @@ import { nanoid } from 'nanoid'
 
     const [dies, setDies] = React.useState(
       // State Lazy init, by using an arrow function
-      () => randomArray().map(el => ({id: nanoid(), value:el})))
+      () => randomArray().map(el => ({id: nanoid(), value:el, isHeld: false})))
 
     console.log("dies: ", dies)
 
     const rollDice = () => {
       // console.log("before roll: ", dies)
       // console.log("roll: ", dies.map(die => ({id:die.id, value:randonNum()})))
-      setDies(oldDies => (oldDies.map(die => ({id:die.id, value:randonNum()}))))
+      setDies(oldDies => (oldDies.map(die => ({id:die.id, value:randonNum(), isHeld:die.isHeld}))))
       // console.log("after roll: ", dies)
     }
 
@@ -46,7 +27,7 @@ import { nanoid } from 'nanoid'
           <div className='dice-container'>
             {dies.map(die => (<Die value={die.value} key={die.id}/>))}
           </div>
-          <button onClick={rollDice} className="roll-dice-button">Roll</button>
+          <button onClick={rollDice} className="roll-dice">Roll</button>
         </main>
     )
   }
